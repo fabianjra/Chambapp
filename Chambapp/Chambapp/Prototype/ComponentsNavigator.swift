b8919516b1219a6431bf34c89a3f3b165d6e518a
Fabian Rodriguez <fabianjra@hotmail.com>
2024-05-22 19:41:14 -0600

    Add Color Sets.

---
+++ Chambapp/Chambapp/Prototype/ComponentsNavigator.swift
@@ -0,0 +1,18 @@
+//
+//  ComponentsNavigator.swift
+//  Chambapp
+//
+//  Created by Fabian Rodriguez on 22/5/24.
+//
+
+import SwiftUI
+
+struct ComponentsNavigator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComponentsNavigator()
+}
